cb7338bbae963fa0d7aa17b2d0494a73a96daed8
ryanyuri357 <ryan.yuri357@gmail.com>
2021-11-05 21:34:18 -0500

    Skills typo

--- src/portfolio.js
+++ src/portfolio.js
@@ -40,7 +40,7 @@ const skillsSection = {
     emoji("⚡ Efficient Back end Development with Node.js + Express"),
     emoji("⚡ Unit + Integration Testing, Test Driven Development"),
     emoji(
-      "⚡ SQL and non-SQL Database Integration"
+      "⚡ SQL and noSQL Database Integration"
     ),
     emoji(
         "⚡ Agile/Scrum"
